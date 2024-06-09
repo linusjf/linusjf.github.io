0a467894aab5b912019dd6e0a8745d57d307d72e
Linus Fernandes <linus.fernandes@gmail.com>
2024-06-09 21:48:52 +0530

    fix: update for mjs

--- DrumMachine/importmap.js
+++ DrumMachine/importmap.js
@@ -3,7 +3,7 @@ im.type = "importmap";
 im.textContent = JSON.stringify({
   imports: {
     react: "https://esm.sh/react@18.2.0",
-    "react-dom/client": "https://esm.sh/react-dom/client@18.2.0",
+    "react-dom/client": "https://esm.sh/react-dom@18.2.0/client",
     "react-bootstrap": "https://esm.sh/react-bootstrap@2.10.2",
     drummachine: "/DrumMachine/drummachine.mjs",
   },
